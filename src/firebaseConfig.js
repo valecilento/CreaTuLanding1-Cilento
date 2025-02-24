c3a87b6bf8eec1cb764f910b90edf674ca4b5cb7
Valeria Cilento <valeecilento@gmail.com>
2025-02-24 16:40:48 -0300

    Se crean variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC15h5ucdJjjNu1ldlRwCRwVZG6-kXQYPs",
-  authDomain: "proyect-react-65499.firebaseapp.com",
-  projectId: "proyect-react-65499",
-  storageBucket: "proyect-react-65499.firebasestorage.app",
-  messagingSenderId: "824207857324",
-  appId: "1:824207857324:web:a3782e47d898ade3a33109"
+  apiKey: import.meta.env.VITE_API_EY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROYECTID,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
